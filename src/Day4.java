60d0964632579437cd0a294bceafeca163194262
Lucinde <lucinde@lucinde.nl>
2023-04-15 13:48:30 +0200

    Day 4 - part A and B

--- src/Day4.java
+++ src/Day4.java
@@ -16,7 +16,8 @@ public class Day4 {
         ArrayList<String[]> sections = getSections(results);
         //System.out.println(sections);
         int numbers = getNumbers(sections);
-        System.out.println("Assignment pairs that fully contain each-others range: " + numbers);
+        //switch for loop comments to see the first assignment and check how many fully contain each-others range
+        System.out.println("Assignment pairs that overlap: " + numbers);
     }
 
     private void processInput() throws IOException {
@@ -40,21 +41,30 @@ public class Day4 {
         ArrayList<Integer> numbers = new ArrayList<>();
         int range = 0;
         for (String[] section : sections) {
-            //System.out.println(section);
+//            System.out.println(section);
             for (int i = 0; i < section.length; i++) {
                 String[] k = section[i].split("-");
                 numbers.add(Integer.parseInt(k[0]));
                 numbers.add(Integer.parseInt(k[1]));
             }
         }
+        // for loop for first check to see how many fully contain eachothers range
+//        for (int i = 0; i < numbers.size(); i+=4) {
+//            if(numbers.get(i) <= numbers.get(i+2) && numbers.get(i+3) <= numbers.get(i+1) ||
+//                    numbers.get(i) >= numbers.get(i+2) && numbers.get(i+3) >= numbers.get(i+1)) {
+//                range += 1;
+//            }
+//        }
+
+        // for loop to check how many ranges overlap
         for (int i = 0; i < numbers.size(); i+=4) {
-            if(numbers.get(i) <= numbers.get(i+2) && numbers.get(i+3) <= numbers.get(i+1) ||
-                    numbers.get(i) >= numbers.get(i+2) && numbers.get(i+3) >= numbers.get(i+1)) {
+            if(numbers.get(i+2) <= numbers.get(i+1) && numbers.get(i+3) >= numbers.get(i)) {
                 range += 1;
             }
         }
         return range;
     }
+
 
 
 }
